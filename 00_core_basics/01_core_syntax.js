9ca447ca6cef15a69b6391f473fd5a2b2759d4cc
Neo-1042 <rafaelh.71642@gmail.com>
2025-07-02 02:11:21 -0600

    Add notes on naming variables and operators in JS

--- 00_core_basics/01_core_syntax.js
+++ 00_core_basics/01_core_syntax.js
@@ -12,3 +12,10 @@ userName = 'Rodrigo';
 // Use constants as often as possible
 const totalUsers = 10;
 totalUsers = 20; // ERROR, totalUsers' value cannot change
+
+let currentResult = 0; // camelCase for JavaScript is customary
+let current_result = 0; // BAD PRACTICE for JS (snake_case)
+let $variable = 0; // allowed, but I don't like it, because it can be confused with PHP or jQuery
+let _foo = 'FOO'; // I don't like it either
+// let 4var = 0; NOT allowed
+// let var-b = 0; NOT allowed
--- 00_core_basics/basics-01-starting-project/assets/scripts/calculator_app.js
+++ 00_core_basics/basics-01-starting-project/assets/scripts/calculator_app.js
@@ -1,4 +1,8 @@
 'use strict';
 
+// Reminder: a**b = exponentiation operator
 // Adding the calculator logic
 let currentResult = 0;
+
+
+outputResult(currentResult, ''); // from vendor.js
