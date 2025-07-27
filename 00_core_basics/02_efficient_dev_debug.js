b7dd6fa186b31ea61c68b169180fefa05f9ff25d
Neo-1042 <rafaelh.71642@gmail.com>
2025-07-26 21:39:37 -0600

    Add some tips on keyboard shortcuts
    Change some local configurations on editing code

--- 00_core_basics/02_efficient_dev_debug.js
+++ 00_core_basics/02_efficient_dev_debug.js
@@ -1,3 +1,16 @@
-'use strict'
+'use strict';
 
 // Efficient Development and Debugging
+
+/*
+Useful keyboard shortcuts:
+    ctrl + E = Move the cursor to the end of the line
+    ctrl + A = Move the cursor to the start of the line
+    Command + D = Duplicate Current Selection (custom)
+    ctrl + shift + N = Add next occurrence (custom)
+    Command + Shift + 7 = Toggle Line Comment
+    Command + Shift + C = Toggle Block Comment
+
+Installing VS Code Extensions
+*/
+
