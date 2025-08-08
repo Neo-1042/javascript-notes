90faca8318ec56119347a07f008a9a0ffd9822e1
Neo-1042 <rafaelh.71642@gmail.com>
2025-08-07 23:21:12 -0600

    Add STRONG_ATTACK mode, refactor attack functions

--- 00_core_basics/monster-killer-project/assets/scripts/monster_app.js
+++ 00_core_basics/monster-killer-project/assets/scripts/monster_app.js
@@ -1,22 +1,31 @@
 'use strict';
 
+// GLOBAL CONSTANTS AND VARIABLES
 const ATTACK_VALUE = 10; // Global, hard-coded constant (UPPERCASE is a convention)
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 10;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
-// Logic for the "ATTACK" button
-// "*Handler()" is a naming convention, "*OnClick()" is another option
-function attackHandler() {
-    // Decrease monster's health when attacked
-    const damage = dealMonsterDamage(ATTACK_VALUE); // local constant
+// FUNCTION DEFINITIONS
+function attackMonster(mode) {
+    let maxDamage;
+    if (mode === 'ATTACK') {
+        maxDamage = ATTACK_VALUE;
+    } else if (mode === 'STRONG_ATTACK') {
+        maxDamage = STRONG_ATTACK_VALUE;
+    } else {
+        return "Not a valid MODE of attack";
+    }
+    // Attack the monster
+    const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
-    // Monster strikes back
+    // The monster strikes back (based only on one value)
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
 
-    // Have I won?
+    // Who has won?
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert("Congratulations, you won!");
     } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
@@ -27,6 +36,19 @@ function attackHandler() {
 }
 
 
+// Logic for the "ATTACK" button
+// "*Handler()" is a naming convention, "*OnClick()" is another option
+function attackHandler() {
+    attackMonster('ATTACK'); 
+}
+
+function strongAttackHandler() {
+    attackMonster('STRONG_ATTACK');
+}
+
+
 adjustHealthBars(chosenMaxLife);
 
+// Event Listeners on clicking the buttons:
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
