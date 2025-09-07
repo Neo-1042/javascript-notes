5131ab60ace5d7915f9e8ded67267d536365899b
Neo-1042 <rafaelh.71642@gmail.com>
2025-09-06 23:59:35 -0600

    Add notes and quiz on truthy falsy values, double bang operator and boolean coercion

--- 00_core_basics/05_conditional_expr.js
+++ 00_core_basics/05_conditional_expr.js
@@ -21,8 +21,29 @@ const logEvent =
 
 // Boolean coercion via:
 // Double bang operator !! (-1)*(-1) = +1
+// NOT (NOT) Generates a real boolean value
 
 const realBooleanValue = !!1; // Type conversion trick
 
 // Default value assignment via OR operator (||)
 const name = someInput || 'Rodrigo'; // If someInput is empty, then is falsy, so it would default to 'Rodrigo'
+
+const nameo = isLoggedIn && 'Master'; // if and only if user is logged in, set 'nameo' to Master
+
+const userInput = ''; // Undefined
+
+let isValidInput = userInput ? true : false ; // Non-empty 'userInput'
+// Trick: double bang operator
+isValidInput = !!userInput ;  // Real boolean based on whether 'userInput' is false or true.
+
+// Arrays (empty or not) are always treated as 'truthy' values
+let shoppingCart = isLoggedIn && ['Books']; // if user is logged in, the shopping cart will contain 'Books'
+
+// Reminder:
+let enteredValue = '';
+let testValue = enteredValue || 'PLACEHOLDER'; // Since enteredValue is an empty string, testValue will store 'PLACEHOLDER'
+
+testValue = enteredValue || null; // Still, since enteredValue is empty (falsy), testValue will store 'null' (the second value)
+
+enteredValue = 'Max';
+testValue = enteredValue && ''; // Stores '', since the first value is "truthy"
